772286be300eec7e69ded031f071fb961a4bfdc5
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-03-01 13:52:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,7 +43,7 @@ async def start(client, message):
             InlineKeyboardButton('❓How To Use Me', url=f'https://telegram.me/{TUTORIAL}'),
             InlineKeyboardButton('⚜ Updates', url=f'https://telegram.me/{CHNL_LNK}')
         ],[
-            InlineKeyboardButton('🌟 Premium Membership 🌟', callback_data="PREMIUM")
+            InlineKeyboardButton("🌟 Premium Membership 🌟", callback_data="PREMIUM_TXT")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -86,7 +86,7 @@ async def start(client, message):
             InlineKeyboardButton('❓How To Use Me', url=f'https://telegram.me/{TUTORIAL}'),
             InlineKeyboardButton('⚜ Updates', url=f'https://telegram.me/{CHNL_LNK}')
         ],[
-            InlineKeyboardButton('🌟 Premium Membership 🌟', callback_data="PREMIUM")
+            InlineKeyboardButton("🌟 Premium Membership 🌟", callback_data="PREMIUM_TXT")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
