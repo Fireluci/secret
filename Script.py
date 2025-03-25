a1b8552f6d5168f75f0035e5d3f206d380b96685
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-03-25 09:32:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,20 +2,20 @@ class script(object):
     START_TXT = """<i><b>🎃 Helo {}, I'm <a href=https://telegram.me/{}>{}</a></i></b> \n\n<i><b>🔅 I Can Provide You Any Movies, Web-Series, Anime, K-Dramas, Animation, etc!</i></b>"""
     
     
-    HELP_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    HELP_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    ABOUT_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    ABOUT_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    SOURCE_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
-    MANUELFILTER_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    SOURCE_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
+    MANUELFILTER_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    BUTTON_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    BUTTON_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    AUTOFILTER_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    AUTOFILTER_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    CONNECTION_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
-    EXTRAMOD_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
-    ADMIN_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    CONNECTION_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
+    EXTRAMOD_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
+    ADMIN_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
 
     STATUS_TXT = """<b>★ Tᴏᴛᴀʟ Fɪʟᴇs: <code>{}</code>
 ★ Tᴏᴛᴀʟ Usᴇʀs: <code>{}</code>
@@ -42,34 +42,34 @@ Nᴀᴍᴇ - {}"""
 
     I_CUDNT = """<b><i>💢 Sorry – It’s Not Available❗️
 
-⚠️ The Reason❓[<a href="https://telegram.me/HeroFlixx/3">Click Here</a>]
+⚠️ The Reason❓[<a href="https://t.me/MiaFlix/18">Click Here</a>]
 🗨 Please Follow Request Tips!
-🔆 Request Tips ➔ [<a href="https://telegram.me/HeroFlixx/2">Click Here</a>]</i></b>"""
+🔆 Request Tips ➔ [<a href="https://t.me/MiaFlix/17">Click Here</a>]</i></b>"""
 
 
     I_CUD_NT = """<b><i>💢 Sorry – It’s Not Available❗️
 
-⚠️ The Reason❓[<a href="https://telegram.me/HeroFlixx/3">Click Here</a>]
+⚠️ The Reason❓[<a href="https://t.me/MiaFlix/18">Click Here</a>]
 🗨 Please Follow Request Tips!
-🔆 Request Tips ➔ [<a href="https://telegram.me/HeroFlixx/2">Click Here</a>]</i></b>"""
+🔆 Request Tips ➔ [<a href="https://t.me/MiaFlix/17">Click Here</a>]</i></b>"""
 
     MVE_NT_FND = """<b><i>💢 Sorry – It’s Not Available❗️
 
-⚠️ The Reason❓[<a href="https://telegram.me/HeroFlixx/3">Click Here</a>]
+⚠️ The Reason❓[<a href="https://t.me/MiaFlix/18">Click Here</a>]
 🗨 Please Follow Request Tips!
-🔆 Request Tips ➔ [<a href="https://telegram.me/HeroFlixx/2">Click Here</a>]</i></b>"""
+🔆 Request Tips ➔ [<a href="https://t.me/MiaFlix/17">Click Here</a>]</i></b>"""
 
-    TOP_ALRT_MSG = """🔆彡[ @HEROFLiX ]彡🔆"""
+    TOP_ALRT_MSG = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    MELCOW_ENG = """<b><i>🔆 "HEROFLiX • GROUP" 亗 🔆
+    MELCOW_ENG = """<b><i>🔆 "APNAFLiX • GROUP" 亗 🔆
 •───────────────────• 
-⚜Hey {}, Welcome To HEROFlix • Group. You Can Request Any Movies, Web-Series, Anime, K-Dramas, Animation etc., here....</i></b>"""
+⚜Hey {}, Welcome To APNAFLiX • Group. You Can Request Any Movies, Web-Series, Anime, K-Dramas, Animation etc., here....</i></b>"""
 
-    SHORTLINK_INFO = """🔆彡[ @HEROFLiX ]彡🔆"""
+    SHORTLINK_INFO = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    REQINFO = """🔆彡[ @HEROFLiX ]彡🔆"""
+    REQINFO = """🔆彡[ @APNAFLiX ]彡🔆"""
 
-    SINFO = """🔆彡[ @HEROFLiX ]彡🔆"""
+    SINFO = """🔆彡[ @APNAFLiX ]彡🔆"""
 
     NORSLTS = """
 ★ #𝗡𝗼𝗥𝗲𝘀𝘂𝗹𝘁𝘀 ★
@@ -81,7 +81,7 @@ Nᴀᴍᴇ - {}"""
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
     CAPTION = """
- <a href="https://telegram.me/HEROFLiX"><b><i>{file_name}</i></b></a>"""
+ <a href="https://telegram.me/APNAFLiX"><b><i>{file_name}</i></b></a>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {query}
@@ -93,11 +93,11 @@ IMDb Data:
 ⏱️ Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10</b>"""
     
-    ALL_FILTERS = """🔆彡[ @HEROFLiX ]彡🔆"""
+    ALL_FILTERS = """🔆彡[ @APNAFLiX ]彡🔆"""
     
-    GFILTER_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    GFILTER_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
     
-    FILE_STORE_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    FILE_STORE_TXT = """🔆彡[ @APNAFLiX ]彡🔆"""
     RESTART_TXT = """
 <b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
 
