495768360a6f2fac46fe375bc4a239249ca58601
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-04-24 20:28:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '1736204'))
 API_HASH = environ.get('API_HASH', '890d40e0f91a4de32dec2965444b2cbe')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7767163687:AAGqVMH179R357dVGExbrd9cG1qLCpMbrcI')
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://heroflix3bot:heroflix3bot@heroflix3bot.jqdduvb.mongodb.net/?retryWrites=true&w=majority&appName=heroflix3bot")
 DATABASE_NAME = environ.get('DATABASE_NAME', "heroflix3bot")
