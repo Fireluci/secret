29c7d440ec3a36c46aac3206e951e79f6bd7dc22
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-04-26 08:48:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,11 +14,11 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '1736204'))
 API_HASH = environ.get('API_HASH', '890d40e0f91a4de32dec2965444b2cbe')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7767163687:AAGqVMH179R357dVGExbrd9cG1qLCpMbrcI')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://heroflix3bot:heroflix3bot@heroflix3bot.jqdduvb.mongodb.net/?retryWrites=true&w=majority&appName=heroflix3bot")
-DATABASE_NAME = environ.get('DATABASE_NAME', "heroflix3bot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'heroflix3bot')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://carla:carla@carla.8vo6x16.mongodb.net/?retryWrites=true&w=majority&appName=carla")
+DATABASE_NAME = environ.get('DATABASE_NAME', "heroflix")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'heroflix')
 
 DWLD = environ.get("DWLD", 'HeroFlixx/8')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
