febe0077ba8df9f2ed69d0324bf7c7a59581d683
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-07-24 14:03:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,7 +1,6 @@
 import asyncio, re, ast, math, random, pytz, logging
 import time as _time
 from datetime import datetime, timedelta, date, time
-
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
@@ -9,7 +8,7 @@ from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidD
 from html import escape
 from Script import script
 from info import *
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, get_tutorial, send_all
+from utils import get_size, is_subscribed, search_gagala, temp, get_settings, save_group_settings, get_shortlink, get_tutorial, send_all
 from database.users_chats_db import db
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
 from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
