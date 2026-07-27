0e4ed37bf414ae704772be5cc45d851daeceb8e4
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-07-27 12:20:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -46,7 +46,7 @@ async def start(client, message):
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo=PICS,
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
@@ -79,7 +79,7 @@ async def start(client, message):
         ]
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo=PICS,
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
