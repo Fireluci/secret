b1411b66ea0a97e4a99c7d5508311cbfcf671d36
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-07-30 14:31:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ API_ID = int(environ.get('API_ID', '1736204'))
 API_HASH = environ.get('API_HASH', '890d40e0f91a4de32dec2965444b2cbe')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://shin:shin@shin.3x3uyc3.mongodb.net/?retryWrites=true&w=majority&appName=shin")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "heroflix")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'heroflix')
 
