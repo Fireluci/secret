c0e579985761b953e3853098cdb4ba32edc0c99f
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-08-26 14:48:25 +0530

    Change import of start_premium_tasks to autofilter

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
  
 from plugins.index import check_pending_index_on_startup
-from plugins.premium import start_premium_tasks
+from plugins.autofilter import start_premium_tasks
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
