ae020f27468eb99254bf4d9b3a5f821d6767e9ed
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-08-27 18:13:40 +0530

    Refactor bot.py by removing iter_messages method
    Removed unused iter_messages method and added close_http_session call in stop method.

--- bot.py
+++ bot.py
@@ -12,9 +12,7 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from utils import temp
-from typing import Union, Optional, AsyncGenerator
-from pyrogram import types
+from utils import temp, close_http_session
 from datetime import date, datetime 
 import pytz
 from aiohttp import web
@@ -99,24 +97,9 @@ class Bot(Client):
         await check_pending_index_on_startup(self)
 
     async def stop(self, *args):
+        await close_http_session()
         await super().stop()
         logging.info("Bot stopped. Bye.")
-
-    async def iter_messages(
-        self,
-        chat_id: Union[int, str],
-        limit: int,
-        offset: int = 0,
-    ) -> Optional[AsyncGenerator["types.Message", None]]:
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
 
 
 app = Bot()
