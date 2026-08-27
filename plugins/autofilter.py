13453638b79f9e19270f0a8c5f351725cb95b7a2
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-08-27 18:24:53 +0530

    Fix variable reference for message in autofilter.py

--- plugins/autofilter.py
+++ plugins/autofilter.py
@@ -194,7 +194,7 @@ async def next_page(bot, query):
             query.message.chat.id, req, key, offset, next_offset, total
         )
         await store_file_links(query.message.chat.id, files)
-        cap = build_results_caption(query.message.chat.id, search, files)
+        cap = build_results_caption(message.chat.id, search, files)
 
         try:
             await query.message.edit_text(
@@ -372,7 +372,7 @@ async def auto_filter(client, msg, spoll=False):
         message.chat.id, req, key, 0, offset, total_results
     )
     await store_file_links(message.chat.id, files)
-    cap = build_results_caption(query.message.chat.id, search, files)
+    cap = build_results_caption(message.chat.id, search, files)
 
     result = await message.reply_text(
         cap,
