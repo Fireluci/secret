221ada1b43e3bd4b572f56c52135679c054c2478
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-09-08 11:47:02 +0530

    Change default COLLECTION_NAME from 'indexx' to 'index'

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7836793689:AAGxFiWKM4_93vOI6XlRPpHAYJjeJuj
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://minju:minju@minju.0nj8agm.mongodb.net/?appName=minju")
 DATABASE_NAME = environ.get('DATABASE_NAME', "heroflix")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'indexx')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'index')
 
 CAPTION_INDEX_CHANNEL = int(environ.get('CAPTION_INDEX_CHANNEL', '-1002299214709'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001638006524').split()] + [CAPTION_INDEX_CHANNEL]
