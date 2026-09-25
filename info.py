3feb90fdf51a2406b5e0e1b27073351c4c68cb80
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-09-25 09:18:46 +0530

    Update environment variable defaults in info.py

--- info.py
+++ info.py
@@ -13,11 +13,11 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '1736204'))
 API_HASH = environ.get('API_HASH', '890d40e0f91a4de32dec2965444b2cbe')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7836793689:AAGxFiWKM4_93vOI6XlRPpHAYJjeJujqic4')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://minju:minju@minju.0nj8agm.mongodb.net/?appName=minju")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "heroflix")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'index')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'heroflix')
 
 CAPTION_INDEX_CHANNEL = int(environ.get('CAPTION_INDEX_CHANNEL', '-1002299214709'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001638006524').split()] + [CAPTION_INDEX_CHANNEL]
